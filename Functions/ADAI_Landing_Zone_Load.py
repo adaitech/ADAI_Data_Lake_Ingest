2b4e2f19eace40bc43855667530c26a62e778406
Jonatan Cardoso Saraiva <jonatan.saraiva@hdi-yelum.com.br>
2025-06-12 16:49:05 -0300

    create function

---
+++ Functions/ADAI_Landing_Zone_Load.py
@@ -0,0 +1,13 @@
+import logging
+import azure.functions as func
+
+app = func.FunctionApp()
+
+
+@app.timer_trigger(schedule="0 4 * * TUE", arg_name="myTimer", run_on_startup=False,
+                   use_monitor=False)
+def ADAI_Data_Acquisition(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function executed.')
